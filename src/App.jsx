817b7a3f2c62234f4fb7ebd04781ebc84a78f6b4
Alok Mishra <alokmish.pro@gmail.com>
2022-05-05 07:50:07 +0000

    Color Palette Generator web app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import Color from './Color'
+
+import Values from 'values.js'
 
 function App() {
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(new Values('#023e8a').all(10))
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
+  }
+
   return (
-    <main>
-      This is a ReactJS App.
-    </main>
-  );
+    <>
+      <section className='container'>
+        <h3>color palette</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <Color
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
+        })}
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
